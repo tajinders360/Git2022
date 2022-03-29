ebea262803f0415b65872797e347345e19f65997
unknown <tajinder.singh28@gmail.com>
2022-03-29 15:13:56 +0530

    Sec branch commit

--- mavenproject/src/test/java/com/test/Pages/FirstTest.java
+++ mavenproject/src/test/java/com/test/Pages/FirstTest.java
@@ -3,7 +3,7 @@ package com.test.Pages;
 public class FirstTest {
 
 	public static void main(String[] args) {
-		System.out.println("This is first branch");
+		System.out.println("This is second branch");
 
 	}
 
